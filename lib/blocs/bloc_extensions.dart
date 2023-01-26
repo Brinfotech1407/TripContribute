cd3b569d843c1b472fda9af440de5d94ab6a8f9f
Bhavika <itqorconsulting@gmail.com>
2023-01-26 13:40:10 +0530

    # sdk version change and bloc extensions add auth bloc

--- lib/blocs/bloc_extensions.dart
+++ lib/blocs/bloc_extensions.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trip_contribute/blocs/auth_bloc/auth_bloc.dart';
 import 'package:trip_contribute/blocs/ok_done_bloc/ok_done_bloc.dart';
 
 extension BlocExtensions on BuildContext {
   OkDoneBloc get okDoneBloc => BlocProvider.of<OkDoneBloc>(this);
+
+  AuthBloc get authBloc => BlocProvider.of<AuthBloc>(this);
+
 }
